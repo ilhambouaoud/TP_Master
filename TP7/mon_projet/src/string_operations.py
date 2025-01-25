a1cdf44189799fcd291184a3866f4b7815fa9b69
ilhambouaoud <124381324+ilhambouaoud@users.noreply.github.com>
2025-01-25 20:00:40 +0100

    Add files via upload

---
+++ TP7/mon_projet/src/string_operations.py
@@ -0,0 +1,6 @@
+def concatener(chaine1, chaine2):
+    return chaine1 + chaine2 # cette fonction retourne la combinaison de chaîne1 et chaîne2
+
+def mettre_en_majuscules(chaine):
+    return chaine.upper()  #retourner la chaîne convertie en majuscules a l'aide de upper
+
